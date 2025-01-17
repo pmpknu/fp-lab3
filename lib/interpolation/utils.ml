0f2b1f3f3b86162474fa04c923ddd44b5706e373
pmpknu <dangorlyakov@gmail.com>
2025-01-17 15:49:33 +0300

    feat(utils): add generate x range function

--- lib/interpolation/utils.ml
+++ lib/interpolation/utils.ml
@@ -1,2 +1,20 @@
 type point = float * float
 
+let generate_x_range (step : float) (start : point) (target : point) : float list =
+  let (start_x, _) = start in
+  let (target_x, _) = target in
+  let rec aux x acc =
+    if x > target_x +. step then acc
+    else aux (x +. step) (x :: acc)
+  in
+  List.rev (aux start_x [])
+
+let%expect_test "generate_x_range" =
+  let start = (1.57, 1.) in
+  let target = (3.142, 0.) in
+  let x_range = generate_x_range 1. start target in
+  List.iter (fun x -> Printf.printf "%f\n" x) x_range;
+  [%expect {|
+    1.570000
+    2.570000
+    3.570000 |}]
