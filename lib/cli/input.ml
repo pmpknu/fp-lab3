601a0abb1d2d212588b33258f64530b1025962ff
pmpknu <dangorlyakov@gmail.com>
2025-01-17 19:50:14 +0300

    feat(cli): add cli handle functions

---
+++ lib/cli/input.ml
@@ -0,0 +1,18 @@
+open Interpolation.Utils
+
+let point_of_string (s : string) : point =
+  match String.trim s |> String.split_on_char ',' with
+  | x::y::_ -> (float_of_string x, float_of_string y)
+  | _ -> failwith "Invalid point format"
+
+let%test "point_of_string" =
+  point_of_string "1.57, 1" = (1.57, 1.)
+
+let read_point () : point =
+  Sys.catch_break true;
+  try
+    read_line () |> point_of_string
+  with
+  | End_of_file -> failwith "End of file reached"
+  | Sys.Break -> print_endline "Operation interrupted"; exit 1
+  | e -> raise e
---
+++ lib/cli/loop.ml
@@ -0,0 +1,30 @@
+open Interpolation.Utils
+open Interpolation.Lagrange
+open Interpolation.Linear
+open Input
+open Output
+
+let execute_interpolate algo intepolation step points =
+  print_endline (string_of_algo algo);
+  intepolation step points
+  |> string_of_points
+  |> print_endline
+
+let loop (step : float) (algo : algorithm) =
+  let point1 = read_point () in
+  let point2 = read_point () in
+  let points = [point1; point2] in
+
+  let rec looprec (points : point list) =
+    match algo with
+    | Both ->
+        execute_interpolate Linear linear_interpolation step points;
+        execute_interpolate Lagrange lagrange_interpolation step points
+    | Lagrange -> if List.length points > 3 then execute_interpolate algo lagrange_interpolation step points
+    | Linear -> execute_interpolate algo linear_interpolation step points;
+
+    let new_point = read_point () in
+    looprec (points @ [new_point])
+  in
+
+  looprec points
