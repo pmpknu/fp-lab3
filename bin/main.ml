85aa6826084cec5ac4f18d5f001899eb940c1e70
pmpknu <dangorlyakov@gmail.com>
2025-01-17 19:51:19 +0300

    feat(main): add main

--- bin/main.ml
+++ bin/main.ml
@@ -1,3 +1,39 @@
-open Core
+open Interpolation.Utils
+open Cli.Loop
 
-let () = printf "Hello, world!\n"
+let step = ref 1.
+
+let algo = ref Both
+
+let set_step (value : float) =
+  if value < 0. then raise (Arg.Bad "step must be positive")
+  else step := value
+
+let set_algo (value : string) =
+  match value with
+  | "lagrange" -> algo := Lagrange
+  | "linear" -> algo := Linear
+  | _ -> raise (Arg.Bad "invalid algorithm")
+
+let speclist =
+  [
+    ("-step", Arg.Float set_step, "interpolation step");
+    ("-algo", Arg.String set_algo, "interpolation algorithm. possible values: lagrange, linear");
+  ]
+
+let usage_msg = "interpolate -step <float step> [-algo <string algo>]"
+
+let () =
+  Arg.parse speclist (fun _ -> ()) usage_msg;
+
+  print_endline "Введите начальные точки в формате x,y:";
+  try
+    loop !step !algo
+  with
+  | Failure msg ->
+      Printf.printf "Ошибка: %s\n" msg;
+      exit 1
+  | Sys.Break ->
+      print_endline "Программа остановлена пользователем.";
+      exit 0
+
