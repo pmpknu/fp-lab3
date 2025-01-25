33e8f32264902b74678f83157faf82cc108de358
pmpknu <dangorlyakov@gmail.com>
2025-01-25 11:06:27 +0300

    fix(loop): correct lagrange's output

--- lib/cli/loop.ml
+++ lib/cli/loop.ml
@@ -15,34 +15,41 @@ let loop (step : float) (algo : algorithm) =
   let point2 = read_point () in
   let points = [ point1; point2 ] in
 
-  let rec looprec (points : point list) (start : point) =
-    let new_start =
+  let rec looprec (points : point list) (start_linear : point) (start_lagrange : point) =
+    let new_start_linear, new_start_lagrange =
       match algo with
       | Both ->
-          let linear_result = 
-            execute_interpolate Linear linear_interpolation step points start 
+          let linear_result =
+            execute_interpolate Linear linear_interpolation step points start_linear
           in
-          if List.length points > 3 then
-            let lagrange_result =
-              execute_interpolate Lagrange lagrange_interpolation step points start
-            in
-            lagrange_result
-          else
-            linear_result
+          let lagrange_result =
+            if List.length points > 3 then
+              execute_interpolate Lagrange lagrange_interpolation step points start_lagrange
+            else
+              start_lagrange
+          in
+          (linear_result, lagrange_result)
       | Lagrange ->
           if List.length points > 3 then
-            execute_interpolate algo lagrange_interpolation step points start
+            let lagrange_result =
+              execute_interpolate algo lagrange_interpolation step points start_lagrange
+            in
+            (start_linear, lagrange_result)
           else
-            start
-      | Linear -> 
-          execute_interpolate algo linear_interpolation step points start
+            (start_linear, start_lagrange)
+      | Linear ->
+          let linear_result =
+            execute_interpolate algo linear_interpolation step points start_linear
+          in
+          (linear_result, start_lagrange)
     in
     let new_point = read_point () in
-    looprec 
+    looprec
       (if List.length points > 3 then
         List.tl points @ [ new_point ]
-      else 
+      else
         points @ [ new_point ])
-      new_start
+      new_start_linear
+      new_start_lagrange
   in
-  looprec points (List.hd points)
+  looprec points (List.hd points) (List.hd points)
